853b515805ac083218d837207cf61cc9d97db9f1
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-16 20:01:48 -0600

    iteration 4

--- lib/school.rb
+++ lib/school.rb
@@ -1,37 +1,39 @@
+require 'pry'
+
 class School
-  attr_reader :start_time, :hours
+  attr_reader :start_time, :hours, :student_names
 
   def initialize(start_time, hours)
     @start_time = start_time
-    @hours = hours 
-  end
-
-  def hours
-    @hours
-  end
-
-  def name
-    @name = name
-  end
-
-  def student_names
-    []
+    @hours = hours
+    @student_names = []
   end
 
   def add_student_name(name)
-    name.each do |name|
-      student_names << name
-    end
+    @student_names << name
   end
 
   def end_time
-    @start_time + @hours.to_s
+    end_hour = @start_time.to_i + @hours
+    "#{end_hour}:00"
   end
 
-  #def full_time
-    #if @hours > 4
-      #full_time = true
-    #end
-  #end
+  def is_full_time?
+    if @hours >= 4
+      true
+    end
+  end
+
+  def standard_student_names
+    @student_names.map do |name|
+      name.capitalize
+    end
+  end
+
+  def convert_end_time_to_clock_time
+    end_hour = @start_time.to_i + @hours
+    new = end_hour - 12
+    "#{new}:00"
+  end
 
 end
--- test/school_test.rb
+++ test/school_test.rb
@@ -1,72 +1,59 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/school'
+require 'pry'
 
 class SchoolTest < Minitest::Test
-  #Iteration 1 Tests:
+
   def test_it_exists
     school = School.new('9:00', 7)
-
     assert_instance_of School, school
   end
 
-  def test_it_has_start_time
+  def test_it_has_a_start_time
     school = School.new('9:00', 7)
-
     assert_equal '9:00', school.start_time
   end
 
-  def test_it_has_hours_in_school_day
-    skip
+  def test_it_knows_hours_in_a_school_day
     school = School.new('9:00', 7)
-
-    assert_equal 7, school.hours_in_school_day
+    assert_equal 7, school.hours
   end
 
-  def test_it_starts_with_no_student_names
+  def test_it_starts_with_no_students
     school = School.new('9:00', 7)
-    student_names = []
     assert_equal [], school.student_names
   end
 
-  #Iteration 2 Tests:
-  def test_it_can_add_student_names
-    skip
+  def test_it_can_add_students
     school = School.new('9:00', 7)
-    #names = 'Aurora', 'tim', 'megan'
     school.add_student_name('Aurora')
     school.add_student_name('tim')
     school.add_student_name('megan')
-
-    assert_equal ['Aurora', 'tim', 'megan'], school.student_names
+    assert_equal ["Aurora", "tim", "megan"], school.student_names
   end
 
   def test_it_can_calculate_end_time
-    #school1 = School.new('9:00', 7)
-    #school2 = School.new('9:00', 3
-    #assert_equal '16:00', school1.end_time
-    #assert_equal '12:00', school2.end_time
-  end
-
-  #Iteration 3 Tests:
-  def test_is_school_full_time
     school = School.new('9:00', 7)
-    if school.hours > 4
-      full_time = true
-    end
-
-    assert full_time
+    assert_equal "16:00", school.end_time
   end
 
-  def test_student_names_standard
-    school = School.new ('9:00', 7)
-    names = ['Aurora', 'tim', 'megan']
-    expected = names.map do |name|
-      name.capitalize
-    end
+  def test_it_knows_if_full_time
+    school = School.new('9:00', 7)
+    assert_equal true, school.is_full_time?
   end
-    assert_equal['Aurora', 'Tim', 'Megan'], expected
+
+  def test_it_can_standardize_names
+    school = School.new('9:00', 7)
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+    assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
+  end
+
+  def test_can_convert_to_clock_time
+    school = School.new('9:00', 7)
+    assert_equal "4:00", school.convert_end_time_to_clock_time
+  end
 
 end
-
-#Wow, I'm so sorry I didn't finish
